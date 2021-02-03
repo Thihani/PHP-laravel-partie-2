d8c63da4254083eaee391e3ea8b17194ff284a18
Thi Ha Ni DO <thihani.do@le-campus-numerique.fr>
2021-02-03 22:44:10 +0100

    iteration 5

--- resources/views/product-list.blade.php
+++ resources/views/product-list.blade.php
@@ -1,12 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Mon site</title>
-</head>
-<body>
-    <h1>Liste des produits</h1>
-</body>
-</html>
+@extends('layouts.layout')
+
+@section('title', 'Catalogue')
+
+@section('content')
+    <div class = "photo-anne-sophie">
+        <img id = "photo-anne-sophie" class="img-fluid" src={{ asset('images/anne-sophie.png') }} alt="photo de Anne Sophie">
+
+    </div>
+    <h1>Product list</h1>
+@endsection
+
