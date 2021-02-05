461e9899bd6fd22735595c4326d60cff90fc553e
Thi Ha Ni DO <thihani.do@le-campus-numerique.fr>
2021-02-05 16:49:52 +0100

    fini ite 6

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,32 +5,59 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use DB;
 use App\Models\Product;
+use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
     public function showDetail($id)
     {
-        //$product = DB::selectOne('select id, name, price, description from products where id=:id', ['id' => $id]);
-        $product=Product::all('id',$id);
-        //return view('product-detail', ['product' => $product]);
+        $product = Product::find($id);
+        return view('product-detail', ['product' => $product]);
     }
 
-    public function showList()
+    public function showList(Request $request)
     {
+        if ($request->has('sortby')) {
+            if ($request->sortby == 'name') {
+                $products = Product::all()
+                    ->sortBy('name');
+                return view('product-list', ['productlist' => $products]);
+            } elseif ($request->sortby == 'price') {
+                $products = Product::all()
+                    ->sortByDesc('price');
+                return view('product-list', ['productlist' => $products]);
+            }
+        }else{
+            $products = Product::all();
+            return view('product-list', ['productlist' => $products]);
+
+        }
+
+    }
+
+
+    /*public function sort(Request $request)
+    {
+        $name = $request->input('name');
+        $price = $request->input('price');
+        $products=Product::all()->when($name, function ($query,$name){
+
+            return view('product-list',['productlist' => $products]);
+        });
         /*  triés par nom
         $products=Product::all()
         ->sortBy ('name');
         return view('product-list',['productlist' => $products]);
         */
 
-        /*  triés par prix crossant*/
-        $products=Product::all()
-        ->sortBy ('price');
+        /*triés par prix crossant
+      $products=Product::all()
+      ->sortByDesc('price');
+      return view('product-list',['productlist' => $products]);
+
+        $products=Product::orderBy('price','desc')->get();
         return view('product-list',['productlist' => $products]);
-
-
-
-    }
+    }*/
 }
 
 
--- resources/views/product-list.blade.php
+++ resources/views/product-list.blade.php
@@ -5,13 +5,31 @@
 @section('content')
     <div class = "photo-anne-sophie">
         <img id = "photo-anne-sophie" class="img-fluid" src={{ asset('images/anne-sophie.png') }} alt="photo de Anne Sophie">
+        <div class="btn-group dropend">
+                <form action="/product" method="get">
+                    <select name="sortby" class="form-select" id="inputGroupSelect04">
+                        <option selected><p class="trier">Trier par</p></option>
+                        <option value="name"><p class="trier">Nom</p></option>
+                        <option value="price"><p class="trier">Prix</p></option>
+                    </select>
+                    <button class="btn btn-outline-secondary" type="submit">Valider</button>
+                </form>
+
+{{--            </ul>--}}
+        </div>
+
     </div>
     <div class="row body-productlist">
         @foreach ($productlist as $product)
             <div class="col product-col">
                 <div class="product">
                     <img class="photoproduct" src={{ asset('images/image.png') }} alt="photo of product">
-                    <p>{{ $product->name }} - €{{ $product->price/100 }}</p>
+                    @if ($product->price/100 <= 20)
+                        <p class="bestprice">Best price <br></p>
+                        <p>{{ $product->name }} - €{{ $product->price/100 }}</p>
+                    @else
+                        <p>{{ $product->name }} - €{{ $product->price/100 }}</p>
+                    @endif
                 </div>
             </div>
         @endforeach
